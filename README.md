5c087ce95e79535e7adc2f1639124c0c1ddd06a1
Brainy0789 <bpurdy12@icloud.com>
2025-12-02 19:44:38 -0800

    Initial Source

---
+++ README.md
@@ -0,0 +1,25 @@
+# ChromaSave
+
+ChromaSave is a simple way to store save data using JSON!
+
+## Usage
+
+``` haxe
+import save.ChromaSave;
+
+class Main
+{
+    static function main()
+    {
+        var chromaSave = new ChromaSave('save'); //the field is the filename saved in app data
+
+        chromaSave.parse(); //parses the json and initializes the save data
+
+        chromaSave.set('field', true); //add a field to the save data
+
+        Sys.println(chromaSave.get('field')); //outputs 'true'
+
+        //There are other functions, but they aren't currently documented, as they are not not needed to use the system effectively.
+    }
+}
+```
---
+++ src/save/ChromaSave.hx
@@ -0,0 +1,61 @@
+package save;
+
+import lime.system.System;
+import sys.FileSystem;
+import sys.io.File;
+import haxe.ds.Map;
+import Reflect;
+import haxe.Json;
+
+class ChromaSave
+{
+    public var data:Map<String, Dynamic>;
+    public var saveFile:String;
+    public var saveDir:String;
+
+    public function new(save:String)
+    {
+        saveDir = System.applicationStorageDirectory;
+        saveFile = saveDir + save + ".json";
+
+        if (!FileSystem.exists(saveDir)) {
+            FileSystem.createDirectory(saveDir);
+        }
+
+        data = new Map<String, Dynamic>();
+    }
+
+    public function read():String {
+        if (FileSystem.exists(saveFile)) {
+            return File.getContent(saveFile);
+        }
+        return null;
+    }
+
+    public function write(contents:String) {
+        File.saveContent(saveFile, contents);
+    }
+
+    public function parse():Void
+    {
+        var raw:String = read();
+        if (raw == null) {
+            data = new Map();
+            return;
+        }
+
+        var parsed:Dynamic = Json.parse(raw);
+        data = new Map<String, Dynamic>();
+
+        for (key in Reflect.fields(parsed)) {
+            data.set(key, Reflect.field(parsed, key));
+        }
+    }
+
+    public inline function set(key:String, v:Dynamic) data.set(key, v);
+    public inline function get(key:String) data.get(key);
+
+    public function save():Void {
+        write(Json.stringify(data, "\t"));
+    }
+}
